c886b373ca99658f22693766406579c2a54e849f
aleibo92 <aleibo92@gmail.com>
2019-11-03 20:36:33 +0000

    lesson 20

--- spec/controllers/tasks_controller_spec.rb
+++ spec/controllers/tasks_controller_spec.rb
@@ -26,4 +26,15 @@ RSpec.describe TasksController, type: :controller do
       expect(task.done).to eq(true)
     end
   end
+
+  describe "task#create action" do 
+    it "should allow users to create a task" do
+      post :create, params: {task: {title: "Fix things"}}
+      expect(response).to have_http_status(:success)
+      response_value = ActiveSupport::JSON.decode(@response.body)
+      expect(response_value['title']).to eq("Fix things")
+      expect(Task.last.title).to eq("Fix things")
+    end
+  end
+
 end
